d816aa961e41fa083c7b432b52bc80c26d1f004e
gong.rg <2nEoluUva0ED520>
2021-01-11 20:22:33 +0800

    真常获取数据，可存数据入库，vue加入表单规则

--- common/views.py
+++ common/views.py
@@ -9,42 +9,39 @@ from common.models import User
 # 注册处理
 def customer_register(request):
 
-    # return JsonResponse({'ret': 0})
-
     # 检测是否是option请求
     if request.method == "OPTIONS":
         print("检测到OPTIONS")
         return HttpResponse("ok")
     else:
-        # 从post请求中获取代码
+        # 从post请求中获取数据
+        # 从QueryDict中取出数据，这个方法有点沙雕，需要改进
         data = request.POST.lists()
-
-        name = request.POST.get("name")
-        password = request.POST.get("password")
-        email = request.POST.get("email")
-        tel = request.POST.get("tel")
-        address = request.POST.get("address")
-
-        # data1 = data.keys()
         for d in data:
             d1 = d
+        data = d1[0]
+        data = json.loads(data)
 
-        d2 = d1[0]
-        d3 = d2
-        print(d3)
-        print(type(d3))
+        name = data['name']
+        password = data['password']
+        email = data['email']
+        tel = data['tel']
+        address = data['address']
 
-        # print(type(data))
-        # print(name)
-        if not name:
-            # print("name为空")
-            return JsonResponse({'ret': 0}, status=403)
-        else:
+        try:
             customer = User.objects.create(user_name=name,
                                            password=password,
                                            is_super=1,
                                            email=email,
                                            tel=tel,
                                            address=address)
-
+        except ValueError:
+            print("插入数据失败！！！")
+            return JsonResponse({'ret': 1}, status=500)
+        else:
             return JsonResponse({'ret': 0, 'id': customer.id})
+
+
+# 登录处理
+def customer_login(request):
+    pass
